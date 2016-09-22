d2ad37d5c874565785bf6d6bd28b791bdef7d476
Michael Borsellino <michaelborsellino@gmail.com>
2016-09-22 17:01:11 -0400

    Put execution cycle into function for easier forking

--- shelldriver.cpp
+++ shelldriver.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 char rwBuff[1024];
 
+void allCommands(vector<string> tokens, string command);
 int main(int argc, char* argv[])
 {
 	//Begin program loop
@@ -18,7 +19,7 @@ int main(int argc, char* argv[])
 	{
 		vector<string> tokens, tokensMain;
 		string command = "";
-		string tempBuff = "";
+		//string tempBuff = "";
 		int fds[2];
 		cout<<"$ ";
 
@@ -32,129 +33,147 @@ int main(int argc, char* argv[])
 		{
 			tokensMain.push_back(tempT);
 		}
-		//fork and give each child a single command
-		pipe(fds);
-		write(fds[1],tokensMain[0],1024);
-		for(int d = 0; d < tokensMain.size(); d++)
+		if(tokensMain.size() > 1)
 		{
-			int *status;
-			int pid2 = fork();
-			if(pid2 == 0)
+			//fork and give each child a single command
+			int pid2;
+			pipe(fds);
+			
+			for(int d = 0; d < tokensMain.size(); d++)
 			{
-			}
-			else
-			{
-				wait(status)
-				//if there are no more commands, print out the output
-				if (d == tokensMain.size() - 1)
+				int *status;
+				
+				//If child process, assign the new small command and then break from the main loop
+				
+				if(d % 2 == 0)
 				{
-					cout<<read(fds[0],1024)<<endl;
-
+					fork();
+					close(fds[1]);
+					close(STDIN_FILENO);
+					dup(fds[0]);
+					command = tokensMain[d];
+					break;
 				}
-				//If there are, write the result to the pipe
 				else
 				{
-					write(fds[1],read(fds[0],1024),1024);
+					fork();
+					close(fds[0]);
+					close(STDOUT_FILENO);
+					dup(fds[1]);
+					command = tokensMain[d];
+					break;
 				}
+				
 			}
-		}
-		//read and write back and forth from the child to the parent
-
-		//Create list of words in command line
-		stringstream commandStream(command);
-		while(commandStream >> tempBuff)
-		{
-			tokens.push_back(tempBuff);
-		}
-		//****************************************Start of built-ins*****************************
-		//Built-in for exit
-		if(tokens[0] == "exit")
-			return 0;
-		//Built-in for cd
-		else if(tokens[0] == "cd")
-		{
-			//if there is no /, checks current directory
-			if(tokens[1][0] != '/')
-				tokens[1].insert(0,"./");
-			chdir(tokens[1].c_str());
-		}
-		//Built-in for pwd
-		//Might need to account for path names that are really long. solved on getcwd opengroup
-		else if(tokens[0] == "pwd")
-		{ 
-			char* buffer;
-
-			size_t size = 1024;
-			buffer = (char*)malloc(size);
-			string current(getcwd(buffer,size));
-			free(buffer);
-			cout<<current<<endl;
 
 		}
-		//Built-in for set
-		else if(tokens[0] == "set")
-		{
-			char* tempChar = new char[tokens[1].size() + 1];
-			strcpy(tempChar,tokens[1].c_str());
-			putenv(tempChar);
-		}
-		//************************************Start of User Program Execution Block************
 		else
 		{
-			//converting vector of strings to array of character arrays
-			int currentS = tokens.size() + 1;
-			char* stringList[currentS];
-			for(unsigned int i = 0; i < tokens.size(); i++)
-			{
-				if(stringList[i] != NULL)
-				{
-					stringList[i] = new char[tokens[i].size() + 1];
-					strcpy(stringList[i],tokens[i].c_str());
-				}
-			}
-			//Null Terminating string list for exec
-			stringList[tokens.size()] = NULL;
-			//Combining search paths if MYPATH exists
-			string pathStuff = getenv("PATH");
-			if(getenv("MYPATH") != NULL)
-			{
-				pathStuff += ':';
-				pathStuff += getenv("MYPATH");
-			}
-			//Convert it to stream for easy parsing
-			stringstream pathInfo(pathStuff);
-			
-			string tempToks;
-			DIR *currentdir = NULL;
-			struct dirent *info = NULL;
-			int flag = 0;
-			//Search through all PATH and MYPATH directories to look for user program
-			while(getline(pathInfo,tempToks,':'))
-			{
-				currentdir = opendir(tempToks.c_str());
-				while(info = readdir(currentdir))
-				{
-					//If the user program is found, fork current process and execute it
-					if(info->d_name == tokens[0])
-					{
-						int pid = fork();
-						int *status;
-						if(pid == 0)
-						{
-							tempToks+='/'+tokens[0];
-							execve(tempToks.c_str(), stringList,NULL);
-						}
-						else
-							wait(status);
-						//Program was found, break execution
-						flag = 1;
-						break;
-					}
-				}
-				if(flag == 1)
-					break;
-			}
+			command = tokensMain[0];
+			allCommands(tokens, command);
 		}
 	}
 	return 0;
+}		
+void allCommands(vector<string> tokens, string command)
+{
+	string tempBuff = "";
+	//Create list of words in command line
+	stringstream commandStream(command);
+	while(commandStream >> tempBuff)
+	{
+		tokens.push_back(tempBuff);
+	}
+	//****************************************Start of built-ins*****************************
+	//Built-in for exit
+	if(tokens[0] == "exit")
+		exit(0);
+	//Built-in for cd
+	else if(tokens[0] == "cd")
+	{
+		//if there is no /, checks current directory
+		if(tokens[1][0] != '/')
+			tokens[1].insert(0,"./");
+		chdir(tokens[1].c_str());
+	}
+	//Built-in for pwd
+	//Might need to account for path names that are really long. solved on getcwd opengroup
+	else if(tokens[0] == "pwd")
+	{ 
+		char* buffer;
+
+		size_t size = 1024;
+		buffer = (char*)malloc(size);
+		string current(getcwd(buffer,size));
+		free(buffer);
+		cout<<current<<endl;
+		//write(fds[1],current,1024);
+
+	}
+	//Built-in for set
+	else if(tokens[0] == "set")
+	{
+		char* tempChar = new char[tokens[1].size() + 1];
+		strcpy(tempChar,tokens[1].c_str());
+		putenv(tempChar);
+	}
+	//************************************Start of User Program Execution Block************
+	else
+	{
+		//converting vector of strings to array of character arrays
+		int currentS = tokens.size() + 1;
+		char* stringList[currentS];
+		for(unsigned int i = 0; i < tokens.size(); i++)
+		{
+			if(stringList[i] != NULL)
+			{
+				stringList[i] = new char[tokens[i].size() + 1];
+				strcpy(stringList[i],tokens[i].c_str());
+			}
+		}
+		//Null Terminating string list for exec
+		stringList[tokens.size()] = NULL;
+		//Combining search paths if MYPATH exists
+		string pathStuff = getenv("PATH");
+		if(getenv("MYPATH") != NULL)
+		{
+			pathStuff += ':';
+			pathStuff += getenv("MYPATH");
+		}
+		//Convert it to stream for easy parsing
+		stringstream pathInfo(pathStuff);
+		
+		string tempToks;
+		DIR *currentdir = NULL;
+		struct dirent *info = NULL;
+		int flag = 0;
+		//Search through all PATH and MYPATH directories to look for user program
+		while(getline(pathInfo,tempToks,':'))
+		{
+			currentdir = opendir(tempToks.c_str());
+			while(info = readdir(currentdir))
+			{
+				//If the user program is found, fork current process and execute it
+				if(info->d_name == tokens[0])
+				{
+					int pid = fork();
+					int *status;
+					if(pid == 0)
+					{
+						tempToks+='/'+tokens[0];
+						execve(tempToks.c_str(), stringList,NULL);
+					}
+					else
+						wait(status);
+					//Program was found, break execution
+					flag = 1;
+					break;
+				}
+			}
+			if(flag == 1)
+				break;
+		}
+	}
+	
+
 }
